c17c50baee0a0f1eb9dc93f413b416ec7d760f1d
ege türker <183089834+1etu@users.noreply.github.com>
2026-08-13 18:57:49 +0300

    add(ui): placeholder view for unbuilt pages

---
+++ src/Alfred.App/Views/PlaceholderView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Alfred.App.Views;
+
+public partial class PlaceholderView : UserControl
+{
+    public PlaceholderView()
+    {
+        InitializeComponent();
+    }
+}
